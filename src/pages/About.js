151ffce376b81e94a1b777d17244d14398134a90
Elisee-des <esabidani@gmail.com>
2022-04-10 01:56:44 +0000

    __Logo_&_installation_of_components_is_OK__

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>Apropos</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, quos officia esse blanditiis dignissimos laboriosam repudiandae nesciunt similique ipsum praesentium debitis et asperiores commodi impedit assumenda quo perferendis. Accusantium odit, consectetur cupiditate hic, sint vero voluptatibus amet soluta libero reiciendis, debitis itaque recusandae nobis nam esse corrupti officiis. Fuga quas ad eveniet dignissimos tenetur voluptatem nobis facere omnis officia similique quisquam aliquam sequi asperiores suscipit illo, iusto, facilis tempora incidunt corporis nam odit exercitationem! Accusamus, culpa suscipit inventore dolorum nam blanditiis voluptate saepe dolorem incidunt. Libero qui veritatis ipsum hic omnis perferendis ab, expedita, similique animi natus velit harum ullam?</p>
+        </div>
+    );
+};
+
+export default About;
